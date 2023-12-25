d25bfcfb3b34a795ce01f06a4fbf87b90948a004
Edy31 <you@example.com>
2023-12-25 05:43:33 +0100

    valami5

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,8 +7,10 @@ import java.util.List;
 
 public class Main {
 
+
     public static void main(String[] args) {
         System.out.println("alma");
+        System.out.println("alma2");
     }
     static String conferencePicker(String[] citiesVisited, String[] citiesOffered) {
         return Arrays.stream(citiesOffered).filter(c -> !Arrays.asList(citiesVisited).contains(c)).findFirst().orElse("No worthwhile conferences this year!");
